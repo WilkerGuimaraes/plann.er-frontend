6500e6fd35fe3e60576d366f9e11c678429c6028
Wilker Guimaraes <wilkerguimaraes.07@gmail.com>
2024-07-09 20:41:31 -0300

    refactor: create ConfirmTripModal component

--- src/page/create-trip/index.tsx
+++ src/page/create-trip/index.tsx
@@ -3,13 +3,13 @@ import {
   Calendar,
   MapPin,
   Settings2,
-  User,
   UserRoundPlus,
-  X,
 } from "lucide-react";
 import { FormEvent, useState } from "react";
 import { useNavigate } from "react-router-dom";
+
 import { InviteGuestsModal } from "./invite-guests-modal";
+import { ConfirmTripModal } from "./confirm-trip-modal";
 
 export function CreateTripPage() {
   const navigate = useNavigate();
@@ -70,7 +70,9 @@ export function CreateTripPage() {
     setEmailsToInvite(newEmailList);
   }
 
-  function createTrip() {
+  function createTrip(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
     navigate("/trips/123");
   }
 
@@ -183,63 +185,10 @@ export function CreateTripPage() {
       )}
 
       {isConfirmTripModalOpen && (
-        <div className="fixed inset-0 flex items-center justify-center bg-black/60">
-          <div className="shadow-shape w-[640px] space-y-5 rounded-xl bg-zinc-900 px-6 py-5">
-            <div className="space-y-2">
-              <div className="flex items-center justify-between">
-                <h2 className="text-lg font-semibold">
-                  Confirmar criação de viagem
-                </h2>
-
-                <button type="button" onClick={closeConfirmTripModal}>
-                  <X className="size-5 text-zinc-400" />
-                </button>
-              </div>
-
-              <p className="text-sm text-zinc-400">
-                Para concluir a criação da viagem para{" "}
-                <span className="font-semibold text-zinc-100">
-                  Florianópolis, Brasil
-                </span>{" "}
-                nas datas de{" "}
-                <span className="font-semibold text-zinc-100">
-                  16 a 27 de Agosto de 2024
-                </span>{" "}
-                preencha seus dados abaixo:
-              </p>
-            </div>
-
-            <form onSubmit={addNewEmailToInvite} className="space-y-3">
-              <div className="flex h-14 items-center gap-2 rounded-lg border border-zinc-800 bg-zinc-950 px-4">
-                <User className="size-5 text-zinc-400" />
-                <input
-                  type="text"
-                  name="name"
-                  placeholder="Seu nome completo"
-                  className="flex-1 bg-transparent text-lg placeholder-zinc-400 outline-none"
-                />
-              </div>
-
-              <div className="flex h-14 items-center gap-2 rounded-lg border border-zinc-800 bg-zinc-950 px-4">
-                <User className="size-5 text-zinc-400" />
-                <input
-                  type="text"
-                  name="name"
-                  placeholder="Seu e-mail pessoal"
-                  className="flex-1 bg-transparent text-lg placeholder-zinc-400 outline-none"
-                />
-              </div>
-
-              <button
-                onClick={createTrip}
-                type="submit"
-                className="flex h-11 w-full items-center justify-center gap-2 rounded-lg bg-lime-300 px-5 font-medium text-lime-950 hover:bg-lime-400"
-              >
-                Confirmar criação da viagem
-              </button>
-            </form>
-          </div>
-        </div>
+        <ConfirmTripModal
+          closeConfirmTripModal={closeConfirmTripModal}
+          createTrip={createTrip}
+        />
       )}
     </div>
   );
