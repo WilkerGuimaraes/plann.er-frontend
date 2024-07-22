b94a75e11cde69f362b21581a75a73e600e8ea16
Wilker Guimaraes <wilkerguimaraes.07@gmail.com>
2024-07-22 17:09:43 -0300

    feat: add react-query and react-hook-form for state management and form validation

--- src/app.tsx
+++ src/app.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import { CreateTripPage } from "./page/create-trip";
 import { TripDetailsPage } from "./page/trip-details";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const router = createBrowserRouter([
   {
@@ -14,6 +15,12 @@ const router = createBrowserRouter([
   },
 ]);
 
+const queryClient = new QueryClient();
+
 export function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  );
 }
